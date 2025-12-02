4da04907f5bb6850ec57859d5489b72c0e7824c3
ThomasGk13 <todoridos@gmail.com>
2025-12-03 00:49:52 +0200

    feat: add modapp

---
+++ src/gr/aueb/cf/testgit/ModApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.testgit;
+
+public class MobApp {
+    
+    public static void main(String[] args) {
+        System.out.println(mod( 10, 5));
+    }
+    
+    public static int mod(int a, int b) {
+        return a % b;
+    }
+}
