bf83710abc4d6bab886a55d303ae6034feb1af10
ThomasGk13 <todoridos@gmail.com>
2025-12-03 00:34:48 +0200

    feat: add mulapp

--- src/gr/aueb/cf/testgit/MulApp.java
+++ src/gr/aueb/cf/testgit/MulApp.java
@@ -2,7 +2,7 @@ package gr.aueb.cf.testgit;
 
 public class MulApp {
     public static void main(String[] args) {
-        System.out.println(multiply( 4, 5));
+        System.out.println(multiply( 3, 5));
     }
     
     public static int multiply(int a, int b) {
