a33b0850b4d6ddef250cf8f9cb88bb99633c7888
ThomasGk13 <todoridos@gmail.com>
2025-12-03 02:16:34 +0200

    fix: main fixes

--- src/gr/aueb/cf/testgit/HelloWorld.java
+++ src/gr/aueb/cf/testgit/HelloWorld.java
@@ -3,10 +3,6 @@ package gr.aueb.cf.testgit;
 public class HelloWorld {
     
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello Coding!");
-        System.out.println("Hello Coding Festival!!!!");
-        System.out.println("Hello Coding Factory!!!!!");
         System.out.println("Hello Coding Factory 2021!!!!!");
     }
 }
